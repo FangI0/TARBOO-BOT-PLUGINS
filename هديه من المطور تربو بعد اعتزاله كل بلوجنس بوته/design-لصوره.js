c5accd687b8cedb42cb5c28082a833743a68547c
FangI0 <mohamedking170170170@gmail.com>
2025-01-05 06:42:07 +0200

    Update design-لصوره.js

--- هديه من المطور تربو بعد اعتزاله كل بلوجنس بوته/design-لصوره.js
+++ هديه من المطور تربو بعد اعتزاله كل بلوجنس بوته/design-لصوره.js
@@ -1,17 +1,17 @@
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `✳️ قم بالرد على ملصق بـ:\n\n *${usedPrefix + command}*`
+    const notStickerMessage = `*🪷 قم بالرد على ملصق بـ:*\n\n *${usedPrefix + command}*`
     if (!m.quoted) throw notStickerMessage
     const q = m.quoted || m
     let mime = q.mediaType || ''
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ ها هو*', m)
+    await conn.sendFile(m.chat, out, 'out.png', '*✅ ها هي الصورة*', m)
 }
 handler.help = ['toimg <sticker>']
 handler.tags = ['sticker']
-handler.command = ['لصوره', 'jpg', 'aimg'] 
+handler.command = ['لصوره', 'jpg', 'لصورة'] 
 
 export default handler
