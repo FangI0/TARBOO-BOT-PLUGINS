407d71098be8eb9f43c33f4afa9bad2fbf05ad13
FangI0 <mohamedking170170170@gmail.com>
2025-01-06 03:38:10 +0200

    Update fun-_بايدن.js

--- هديه من المطور تربو بعد اعتزاله كل بلوجنس بوته/fun-_بايدن.js
+++ هديه من المطور تربو بعد اعتزاله كل بلوجنس بوته/fun-_بايدن.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* تربو انت فين+انا هنا`
+  let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* انت فين`
 
   if (!text) throw too
   let lr = (`https://api.popcat.xyz/biden?text=${encodeURIComponent(text)}`)
-  conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه 𝑻𝑨𝑹𝑩𝑶𝑶`, m)
+  conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه Ⱥ𝕌ℝO҉ɌȺ-乃oｲ🌸`, m)
 }
 handler.help = ['drake']
 handler.tags = ['maker']
